abc709722bfbc89b367e8cb4c0d876c62ec5cc94
Upendra Upadhyay <Upendra.Upadhyay.97@gmail.com>
2018-06-21 13:08:35 +0530

    added catch all to connect react

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', authRouter);
 app.use('/order', orderRouter);
 app.use('/user', userRouter);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
