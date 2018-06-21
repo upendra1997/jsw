dec2dcdac43113922b6e088c9cfc575401905b58
Upendra Upadhyay <Upendra.Upadhyay.97@gmail.com>
2018-06-21 13:13:53 +0530

    configured express to serve static files from react

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(cors());
 app.use(fileUpload());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', authRouter);
