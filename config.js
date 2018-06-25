7fe2524c2a0bc8b6994d4091de01fc4ecc60d677
Upendra Upadhyay <Upendra.Upadhyay.97@gmail.com>
2018-06-25 08:11:58 +0530

    save

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const setting = require('./config.json');
 let env = {};
 for (const i in setting) {
-    // process.env[i] = process.env[i] || setting[i];
+    process.env[i] = process.env[i] || setting[i];
     env[i] = process.env[i] || setting[i];
 }
+console.log(process.env);
+console.log(env);
 module.exports = {env};
